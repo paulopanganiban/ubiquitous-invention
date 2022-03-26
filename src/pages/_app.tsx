95316f624731dcabe203ac92590bad0c595dd75c
John Paulo Panganiban <olopangs@gmail.com>
2022-03-26 11:01:09 +0800

    styling: globals.ts add resets

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,7 +4,6 @@ import { GlobalStyles } from "../styles/globals";
 function MyApp({ Component, pageProps }: AppProps) {
   return (
       <>
-      
       <GlobalStyles />
       <Component {...pageProps} />
       </>
